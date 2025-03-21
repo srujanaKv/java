ff7eb7eab88468cd1b18a0526c17ddd431d440db
srujanaKv <srujanakoppadka@gmail.com>
2025-03-21 15:29:37 +0530

    pushing all the code to git

---
+++ Camera.java
@@ -0,0 +1,23 @@
+class Camera{
+public static void main(string[]args){
+int price=25000;
+String name="Nikon";
+System.out.println(price);
+System.out.println(name);
+{
+	float grams=900.7f;
+	byte quantity=2;
+	System.out.println(name);
+	System.out.println(grams);
+	System.out.println(quantity);
+}
+{
+	boolean expired=false;
+	double boxCost=60000;
+	System.out.println(name);
+	System.out.println(price);
+	System.out.println(expired);
+	System.out.println(boxCost);
+}
+}
+}
